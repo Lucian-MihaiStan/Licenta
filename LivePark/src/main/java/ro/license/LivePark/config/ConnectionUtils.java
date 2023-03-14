4d91728edc757c22b9d72db038ccea10d6c5581e
Lucian-MihaiStan <stanlucianmihai@yahoo.com>
2023-03-14 21:08:50 +0200

    adding database

--- LivePark/src/main/java/ro/license/LivePark/config/ConnectionUtils.java
+++
@@ -1,9 +0,0 @@
-package ro.license.LivePark.config;
-
-public interface ConnectionUtils {
-    String FH_LOCAL_3000_URL = "http://localhost:3000";
-    String GET = "GET";
-    String PUT = "PUT";
-    String POST = "POST";
-    String DELETE = "DELETE";
-}
