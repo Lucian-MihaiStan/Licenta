73b92169f0c85ee4118b1b6c7c5b5646ca21a3bc
Lucian-MihaiStan <stanlucianmihai@yahoo.com>
2023-06-23 02:11:04 +0300

    working on css

--- fe/livepark_app/pages/index.tsx
+++ fe/livepark_app/pages/index.tsx
@@ -6,6 +6,7 @@ import { GlobalConstants } from '@/components/globalc_namespace/global-constants
 import { useRouter } from 'next/router'
 import { NavigationBar } from '@/components/navigation_bar/navigation-bar'
 import { useEffect } from 'react'
+import classNames from 'classnames'
 
 const Home: NextPage = () => {
 
@@ -15,14 +16,35 @@ const Home: NextPage = () => {
       routerUtils.push(path);
   }
 
-  useEffect(() => {
-    routerUtils.push(GlobalConstants.LOGIN_FRONTEND_APP_LINK);
-  }, [routerUtils]);
-
   return (
+    <>
+    <Head>
+        <link rel="icon" href="public/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    </Head>
+
     <div>
       
+      <div className={classNames("container-fluid")}>
+        <div className={classNames("row")}>
+
+          <h1>ParkLive</h1>
+
+        </div>
+
+        <div className={classNames("row")}>
+          <div className={classNames("col")}>
+            <button className="btn btn-primary" onClick={(e) => routeToPage(e, GlobalConstants.LOGIN)}>Login</button>
+          </div>
+          <div className={classNames("col")}>
+            <button className="btn btn-primary" onClick={(e) => routeToPage(e, GlobalConstants.REGISTER_FRONTEND_APP_LINK)}>Register</button>
+          </div>
+        </div>
+      </div>
+
     </div>
+    </>
   )
 }
 
